3776001f485f70ba61e39e31dc6fcd982c1b4ac2
ichyo <ichyo00@gmail.com>
2021-08-21 15:59:43 +0900

    fix

---
+++ sql/2_update.sql
@@ -0,0 +1,6 @@
+UPDATE isu_condition SET `condition_level` = CASE
+WHEN CEIL((LENGTH(`condition`) - LENGTH(replace(`condition`, '=true', ''))) / 5) <= 0 THEN 'info'
+WHEN CEIL((LENGTH(`condition`) - LENGTH(replace(`condition`, '=true', ''))) / 5) <= 2 THEN 'warning'
+WHEN CEIL((LENGTH(`condition`) - LENGTH(replace(`condition`, '=true', ''))) / 5) <= 3 THEN 'critical'
+ELSE 'error'
+END;
